63f79e5f1768d63f1e5606783cf57c1159dc32db
Matt Wise <matthew.c.wise@gmail.com>
2024-05-10 09:48:11 -0700

    large sample file (5k samples)

--- data/create_sample.py
+++ data/create_sample.py
@@ -7,8 +7,8 @@ with open(file_path, 'r') as file:
     data = json.load(file)  # Attempt to load the JSON
     
 # # Randomly sample 200 items from the data
-sampled_data = random.sample(data, 200)
+sampled_data = random.sample(data, 5000)
 
 # Output the sampled data as a JSON file
-with open('news_dialogue_sample.json', 'w') as file:
+with open('news_dialogue_large_sample.json', 'w') as file:
     json.dump(sampled_data, file)
