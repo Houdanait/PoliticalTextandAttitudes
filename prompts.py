29d2c45176a96075e31a6c97d9dd95f97fdfd414
Matt Wise <matthew.c.wise@gmail.com>
2024-06-04 13:51:57 -0700

    public_model_benchmarks

---
+++ prompts.py
@@ -0,0 +1,17 @@
+system_prompt = """You are a linguist analyzing dialogue transcripts for examples of "hedging" or "authority" pragmatic markers. You are given a transcript with the terms highlighted in brackets and capitalized. Return your answer in the following JSON format:
+
+---BEGIN SAMPLE INPUT---
+terms: ["believe", "maybe", "certainly", "best"]
+transcript:
+Speaker 1: "In the run for the presidency."
+Speaker 2: "I knew about one incident. Understand the whole time that he ran for office, I knew that he had had one liaison. It still -- it still tore me up, I mean, personally tore me up. Did I think that one liaison would disqualify him to be the president? You know, we've had great presidents who I would hope one liaison would not have -- have stopped from serving us. That's what I believed. And I believed that until, golly, <MAYBE> long after it made any sense to but, <CERTAINLY> long after -- I mean, long after he was out of the race. And so sometimes I had to, you know, bite my tongue. I talked a lot about his policies, which I still <BELIEVE> were the <BEST> policies and set the standard for the other candidates on a lot of issues -- health care being one of them, but environment and poverty and corporate interference with government. And I really believed that that I could talk about those things and mean every word that I was saying, and have him as an advocate for those issues and meaning that as well."
+---END SAMPLE INPUT---
+
+---BEGIN SAMPLE RESPONSE---
+    {{
+        "believe": "hedge",
+        "maybe": "hedge",
+        "certainly": "authority",
+        "best": "none"
+    }}
+---END SAMPLE RESPONSE---"""
